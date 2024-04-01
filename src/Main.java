2e048f2eb3e5c2ebd38bd6bfd09238425444e6ca
Almaz Orymbetov <230585@astanait.edu.kz>
2024-04-01 10:50:59 +0500

    Second task

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+/*
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
@@ -17,6 +18,35 @@ public class Main {
         else
         {
             return n*n+squaresum(n-1);
+        }
+    }
+}*/
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int n = scanner.nextInt();
+        scanner.close();
+        int result = sum(array,n);
+        System.out.println(result);
+    }
+    public static int sum(int[] array, int n)
+    {
+        if (n<=0 || array.length==0)
+        {
+            return 0;
+        }
+        else
+        {
+            return array[n-1] + sum(array, n-1);
         }
     }
 }
